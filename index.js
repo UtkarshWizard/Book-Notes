dfe988d46882691077edc8fa7cc8dcdda4fdcf01
Utkarsh <utkarshr1214@gmail.com>
2024-04-08 17:02:46 +0530

    url change

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ passport.use("google", new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/add",
+    callbackURL: "https://book-notes-xytd.onrender.com/auth/google/add",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
   },
   async (accessToken, refreshToken , profile, cb) => {
